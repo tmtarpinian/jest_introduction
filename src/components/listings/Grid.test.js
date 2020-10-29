d902538514e886c0f4716e1a03c3645d691a0791
ferrisbueller66 <tmtarpinian@gmail.com>
2020-10-28 23:58:25 -0400

    add string matcher to test name values in titles of dataset

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -9,3 +9,13 @@ test('Number of items = 12', ()=>{
 test('Number of items > 12', ()=>{
     expect(numItems).toBeGreaterThanOrEqual(12)
 })
+
+const dataTest = data[0].title;
+
+test('There is a JS in this title', ()=>{
+    expect(dataTest).toMatch(/JS/)
+})
+
+test('The title contains React', ()=>{
+    expect(dataTest).toContain('React')
+})
